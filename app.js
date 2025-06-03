a340506a10905c9344ff23a5d7f78c5dd986a73f
Mohamed Abouzid <mohamed.abouzid@bioinfsys.uni-giessen.de>
2025-06-03 17:42:23 +0200

    fixed routes

--- app.js
+++ app.js
@@ -3,17 +3,27 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
+//connect DB
+const connectDB = require('./db/connect');
+// authentication middleware
+const authenticateUser = require('./middleware/authentication');
+
+// routes
+const authRoutes = require('./routes/auth');
+const jobsRoutes = require('./routes/jobs');
+
+
+
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.use(express.json());
-// extra packages
 
 // routes
-app.get('/', (req, res) => {
-  res.send('jobs api');
-});
+app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/jobs', authenticateUser, jobsRoutes);
+
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
@@ -22,6 +32,7 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
