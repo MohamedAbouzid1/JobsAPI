c5c7718f86d52da85f7931029a85fadaa60854c6
Mohamed Abouzid <mohamed.abouzid@bioinfsys.uni-giessen.de>
2025-06-08 09:54:20 +0200

    added security

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,4 +1,3 @@
-const { CustomAPIError } = require('../errors')
 const { StatusCodes } = require('http-status-codes')
 const errorHandlerMiddleware = (err, req, res, next) => {
 
@@ -6,13 +5,19 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
     msg: err.message || 'Something went wrong try again later',
   }
-
   if (err.name === 'ValidationError') {
     customError.msg = Object.values(err.errors).map((item) => item.message).join(',');
+    customError.statusCode = StatusCodes.BAD_REQUEST
   }
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
+  if (err.name === 'CastError') {
+    customError.msg = `No item found with id: ${err.value}`
+    customError.statusCode = StatusCodes.NOT_FOUND
   }
+  if (err.code && err.code === 11000) {
+    customError.msg = `Duplicate value entered for ${Object.keys(err.keyValue)} field, please choose another value`
+    customError.statusCode = StatusCodes.BAD_REQUEST
+  }
+
   return res.status(customError.statusCode).json({ msg: customError.msg })
 }
 
